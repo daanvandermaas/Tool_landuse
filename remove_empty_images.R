55548e3fa4c276927c446f48579008fad7724bc6
daanvandermaas <daanvandermaas@hotmail.com>
2017-11-23 13:44:03 +0100

    x

---
+++ remove_empty_images.R
@@ -0,0 +1,20 @@
+library(tiff)
+
+dir_images= "/windows_share/output"
+
+subdirs = list.dirs(dir_images, full.names = TRUE, recursive = F)
+
+
+for(dir in subdirs){
+  images = list.files(dir, full.names = TRUE)
+  for(image in images){
+    im = readTIFF(image)
+    if(sum(!im[,,1] > 0.9) < 20  ){
+      file.remove(image)
+    }
+  }
+  
+}
+
+
+
