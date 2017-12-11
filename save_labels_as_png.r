7d82bf5166f05fd1c858e95cfbfd2d6d278eb4ea
daanvandermaas <daanvandermaas@hotmail.com>
2017-12-11 14:07:17 +0100

    x

--- save_labels_as_png.r
+++ save_labels_as_png.r
@@ -1,19 +1,12 @@
-#vind alle subdirs
-#dir_labels = '/home/beheerder/dgxs/Tool_landuse/transfer_20171206/labels'
-#dir_images ='/home/beheerder/dgxs/transfer_20171201'
-# dir_labels = '/media/beheerder/Seagate Backup Plus Drive/selectie_labels'
-# dir_images = '/media/beheerder/Seagate Backup Plus Drive/selectie_images'
-
 source('lib.r')
 source('lees_in.r')
 
-dir_labels = 'db/labels_wegen'
-dir_images = 'db/images_wegen'
-
+dir_labels = 'db/labels_waterdeel'
+dir_images = 'db/images_waterdeel'
+select = c(8)
 
 #selecteer klassen die je wilt gebruiken
 klassen = read_feather('db/dataframe_levels.feather')
-select = c(9)
 klassen = klassen[select,]
 
 
@@ -37,24 +30,36 @@ images = splits[!is.na(splits)]
 
 ##########################################################################################################################
 
-selectie = images
-
-w = 512
-x = lees_in(images = selectie, w = w, klassen = klassen, dir_images = dir_images, dir_labels = dir_labels)
-input = x[[1]]
-labels =  x[[2]]
-
-
 #haal dirs van labels op
 output_dir = 'plaatjes_labels'
 dir.create( file.path('db',output_dir))
-
 dirs = list.dirs(dir_labels, full.names = FALSE, recursive = FALSE)
 for(dir in dirs){  dir.create( file.path('db',output_dir, dir))}
 
-for(i in 1:dim(labels)[1]){
-  writeImage(as.matrix(labels[i,,]),   paste0('db/', output_dir, '/' , images[i], '.png')       )
+
+
+selectie = images
+
+w = 512
+for(i in 1:length(images)){
+  
+  x = lees_in(images = images[i], w = w, klassen = klassen, dir_images = dir_images, dir_labels = dir_labels)
+  labels =  x[[2]]
+  
+
+    writeImage(as.matrix(labels[1,,]),   paste0('db/', output_dir, '/' , images[i], '.png')       )
+  
+  
 }
+
+
+
+
+
+
+
+
+
 
 
 
