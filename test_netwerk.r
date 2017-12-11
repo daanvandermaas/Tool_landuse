453abfbe47592129bbaee6301550bbb52c286a1d
daanvandermaas <daanvandermaas@hotmail.com>
2017-12-11 15:38:39 +0100

    x

--- test_netwerk.r
+++ test_netwerk.r
@@ -1,5 +1,6 @@
 #lees random plaatjes in
 batch_test = 10
+dir_out = 'db/resultaat/plaatjes'
 
 batch = test[ sample(c(1:length(test)), size = batch_test ) ]
 
@@ -9,11 +10,7 @@ plaatjes = lees_in(images = batch, w = w, klassen = klassen, dir_images = dir_im
 prediction = sess$run(out_softmax, feed_dict = dict(x = plaatjes, keep_prob = 1))
 
 for(i in 1:dim(prediction)[1]){
-saveRDS(plaatjes[i,,,],  paste0('db/resultaat/plaatjes_', i, '.rds') )
-  saveRDS(prediction[i,,,1],  paste0('db/resultaat/predict_', i, '.rds') ) 
+saveRDS(plaatjes[i,,,],  paste0(dir_out, '_', i, '.rds') )
+  saveRDS(prediction[i,,,1],  paste0(dir_out, '_', i, '.rds') ) 
 }
 
-for(i in 1:3){
-  display(plaatjes[i,,,])
-  display(prediction[i,,,1])
-}
